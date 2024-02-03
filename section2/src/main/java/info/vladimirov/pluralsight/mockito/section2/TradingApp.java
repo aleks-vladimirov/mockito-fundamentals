f4519be376bacd28658721ceade42bf339a0f615
Aleks Vladimirov <alexandar.vladimirov@gmail.com>
2024-02-03 17:29:35 +0100

    Add the TradingApp class

---
+++ section2/src/main/java/info/vladimirov/pluralsight/mockito/section2/TradingApp.java
@@ -0,0 +1,46 @@
+package info.vladimirov.pluralsight.mockito.section2;
+
+
+import info.vladimirov.pluralsight.mockito.section2.api.AuditRepo;
+import info.vladimirov.pluralsight.mockito.section2.api.Exchange;
+import info.vladimirov.pluralsight.mockito.section2.api.MarketData;
+
+public class TradingApp {
+
+    private final AuditRepo audit;
+    private final MarketData marketData;
+    private final Exchange exchange;
+
+    public TradingApp(AuditRepo audit, MarketData marketData, Exchange exchange) {
+        this.audit = audit;
+        this.marketData = marketData;
+        this.exchange = exchange;
+    }
+
+
+    public boolean executeTradeGuarantee(String symbol, int qty, double price) {
+
+        try {
+            for(int i = 0; i < 10; i++) {
+                boolean executionStatus = marketData.isCurrentOrderPossible(symbol, qty, price);
+
+                if (executionStatus) {
+                    //execute order
+                    if(exchange.execute(symbol, qty, price)) {
+                        audit.reportTrade("Trade for symbol " + symbol + " executed.", qty, price);
+                        return true;
+                    }
+                }
+                Thread.sleep(10);
+            }
+
+        } catch (InterruptedException e) {
+            //intentionally left
+        }
+        audit.reportFailedTrade("Trade for symbol " + symbol + " failed.", qty, price);
+        return false;
+
+    }
+
+
+}
